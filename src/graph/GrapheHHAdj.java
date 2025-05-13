9bf68cd581d620cc203f3bee735164d18de5b9d3
DanyLLL <dany.lim.91210@gmail.com>
2025-05-13 10:40:21 +0200

    Update GrapheHHAdj.java
    Partie 1 complète (ça passe les tests mais rajoutez en pour voir)

--- src/graph/GrapheHHAdj.java
+++ src/graph/GrapheHHAdj.java
@@ -1,25 +1,37 @@
 package graph;
 
-import java.util.List;
+import java.util.*;
 
 public class GrapheHHAdj implements VarGraph {
+    private final HashMap<String, List<Arc<String>>> adj;
 
-	@Override
-	public List<Arc<String>> getSucc(String s) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public GrapheHHAdj() {
+       this.adj = new HashMap<>();
+    }
 
-	@Override
-	public void ajouterSommet(String noeud) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public List<Arc<String>> getSucc(String s) {
+       return adj.getOrDefault(s, Collections.emptyList());
+    }
 
-	@Override
-	public void ajouterArc(String source, String destination, Integer valeur) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void ajouterSommet(String noeud) {
+       adj.putIfAbsent(noeud, new ArrayList<>());
+    }
 
+    @Override
+    public void ajouterArc(String source, String destination, Integer valeur) {
+       ajouterSommet(source);
+       ajouterSommet(destination);
+
+       List<Arc<String>> arcs = adj.get(source);
+
+       // Vérifie si l'arc existe déjà
+       for (Arc<String> arc : arcs) {
+          if (arc.dst().equals(destination)) {
+             throw new IllegalArgumentException("L'arc " + source + " -> " + destination + " existe déjà.");
+          }
+       }
+       arcs.add(new Arc<>(valeur, destination));
+    }
 }
